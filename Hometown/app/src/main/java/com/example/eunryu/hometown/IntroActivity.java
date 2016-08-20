e796e804cce1a16ed20c39518c8f24978ed3c4b9
은류 <instru93@naver.com>
2016-08-20 23:20:05 +0900

    대략 만듬

--- Hometown/app/src/main/java/com/example/eunryu/hometown/IntroActivity.java
+++ Hometown/app/src/main/java/com/example/eunryu/hometown/IntroActivity.java
@@ -2,12 +2,28 @@ package com.example.eunryu.hometown;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
-public class IntroActivity extends AppCompatActivity {
+public class IntroActivity extends AppCompatActivity implements View.OnClickListener{
+
+    Button testBtn;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_intro);
+
+        testBtn = (Button) findViewById(R.id.intro_btn_testBtn);
+        testBtn.setOnClickListener(this);
+    }
+
+    @Override
+    public void onClick(View view) {
+        int id = view.getId();
+
+        if (id == R.id.intro_btn_testBtn) {
+
+        }
     }
 }
---
+++ Hometown/app/src/main/java/com/example/eunryu/hometown/main/MainActivity.java
@@ -0,0 +1,20 @@
+package com.example.eunryu.hometown.main;
+
+import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
+
+import com.example.eunryu.hometown.R;
+
+/**
+ * Created by Eunryu on 16. 8. 20..
+ */
+public class MainActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        
+    }
+}
